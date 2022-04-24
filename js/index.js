f404811c4596e0957686ecfa021dfed328ca5c61
ANJUNI-Sarah <anjuni1996@gmail.com>
2022-04-25 04:00:18 +0800

    RWD-index

--- js/index.js
+++ js/index.js
@@ -13,9 +13,42 @@
     })
 
 
+//hb//
+    $(document).ready(function(){
+        $('.hb').click(function(){
+            if($('.mobile-main-nav-bg').css('display')== 'none'){
+                $('.mobile-main-nav-bg').css('display','block');
+                $('.mobile-main-nav').animate({left:0});}
+            else if($('.mobile-main-nav-bg').css('display')== 'block'){
+                $('.mobile-main-nav').animate({left:-2000});
+                setTimeout(function(){
+                    $('.mobile-main-nav-bg').css('display','none')
+                }, 500);;
+            }
+        })
+    })
+    
+
+//hb-login//
+    $(document).ready(function(){
+        $('#shopbox2').click(function(){
+            $('#login-lightBox').css('display','block')
+            $('.mobile-main-nav-bg').css('display','none')
+            $('.mobile-main-nav').css({left:-2000})
+        });
+        $('#cancel,#login-lightBox').click(function(){
+            $('#login-lightBox').css('display','none')
+        });
+        $('#lightBox').click(function(lightBox){
+            lightBox.stopPropagation();
+        })
+
+    })
+
+
 
 //banner-change//
-    let arr=new Array('1','2','3')
+        let arr=new Array('1','2','3')
         let t=arr.length;
         let j=arr.length-1;
         function change(){
@@ -28,5 +61,6 @@
         }
         time=setInterval(change,7000);
         
-            
+
+
     
